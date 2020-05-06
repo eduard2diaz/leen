0348350e7477f3d049dd88688b14fdf2c856bdb3
eduardo <eduardo9871@gmx.es>
2020-05-05 19:33:30 -0500

    Añadiendo las páginas estáticas

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -25,43 +25,4 @@ class DefaultController extends AbstractController
         return $this->render('default/estatica/'.$page.'.html.twig');
     }
 
-    /**
-     * @Route("/contactanos", name="contactanos")
-     */
-    public function contactanos(Request $request, \Swift_Mailer $mailer)
-    {
-        $form=$this->createForm(ContactoType::class,null,['action'=>$this->generateUrl('contactanos'),'method'=>'POST']);
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
-            $correo=$form->get('correo')->getData();
-            $parameters=['nombre'=>$form->get('nombre')->getData(),
-                         'correo'=>$correo,
-                         'mensaje'=>$form->get('mensaje')->getData(),
-            ];
-            $message = (new \Swift_Message('Xitlali'))
-                ->setFrom($correo)
-                ->setTo($this->getParameter('destiny_email'))
-                ->setBody(
-                    $this->renderView(
-                        'layout/emails/contactanos.html.twig',
-                        $parameters
-                    ),
-                    'text/html'
-                )
-
-                // you can remove the following code if you don't define a text version for your emails
-                ->addPart(
-                    $this->renderView(
-                        'layout/emails/contactanos.txt.twig',
-                        $parameters
-                    ),
-                    'text/plain'
-                );
-
-            $mailer->send($message);
-            $this->addFlash('success','El mensaje fue enviado satisfactoriamente');
-            return $this->redirectToRoute('contactanos');
-        }
-        return $this->render('default/contactanos.html.twig',['form'=>$form->createView()]);
-    }
 }
